7c160466d9ade39137d80f5474ff2fc5c3944ab5
Cody <Cadaeiz@gmail.com>
2013-08-23 00:03:04 -0700

    Initial State (basic game template)

---
+++ Game.cpp
@@ -0,0 +1,33 @@
+#include "Game.hpp"
+#include "GameState.hpp"
+
+void Game::init()
+{
+}
+
+
+void Game::cleanup()
+{
+}
+
+
+void Game::changeState(int state)
+{
+}
+
+
+void Game::handleEvent(sf::Event e)
+{
+	states[currentState] -> handleEvent(*this, e);
+}
+
+void Game::run(float time)
+{
+	states[currentState] -> run(*this, time);
+}
+
+
+void Game::draw(sf::RenderWindow & window)
+{
+	states[currentState] -> draw(*this, window);
+}
